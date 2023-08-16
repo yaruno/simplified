20d3c19c35e8fed6b37d66b757fff0890237649e
Victor Shevtsov <victorshevtsov@gmail.com>
2023-08-16 07:41:47 +0000

    Add external-ip option to the Validator

--- packages/validator/src/cli/startCommand.ts
+++ packages/validator/src/cli/startCommand.ts
@@ -2,12 +2,13 @@ import { CreateClientOptions, createClient } from '@simplified/shared';
 import { Logger } from '@streamr/utils';
 import { Command } from 'commander';
 import { Validator } from '../Validator';
-import { devNetworkOption, privateKeyOption, streamIdOption } from './options';
+import { devNetworkOption, externalIpOption, privateKeyOption, streamIdOption } from './options';
 
 const logger = new Logger(module);
 
 interface Options {
 	devNetwork: boolean;
+	externalIp: string;
 	privateKey: string;
 	streamId: string;
 }
@@ -15,13 +16,15 @@ interface Options {
 export const startCommand = new Command('start')
 	.description('Start Broker')
 	.addOption(devNetworkOption)
+	.addOption(externalIpOption)
 	.addOption(privateKeyOption)
 	.addOption(streamIdOption)
 	.action(async (options: Options) => {
 		logger.info('Starting Validator...');
 
 		const createClientOptions: CreateClientOptions = {
-			devNetwork: options.devNetwork
+			devNetwork: options.devNetwork,
+			externalIp: options.externalIp,
 		}
 
 		const client = await createClient(options.privateKey, createClientOptions);
