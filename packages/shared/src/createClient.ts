0a17ae4373c57b82297beca5b41e30a16e5a4725
Jarno <jarno.marttila@sujuwa.fi>
2023-08-23 15:13:35 +0200

    increase gapfill timeout

--- packages/shared/src/createClient.ts
+++ packages/shared/src/createClient.ts
@@ -25,6 +25,8 @@ export const createClient = async (privateKey: string, options: CreateClientOpti
 
   config.network!.webrtcSendBufferMaxMessageCount = 5000;
   config.network!.webrtcDisallowPrivateAddresses = false;
+  config.gapFill = true
+  config.gapFillTimeout = 30000
 
   if (options.externalIp) {
     config.network!.externalIp = options.externalIp;
